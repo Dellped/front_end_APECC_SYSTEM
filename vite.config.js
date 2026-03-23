b67374d8ff703f8a985299490f7aa8e85ffcab50
IT-8437\RODEL <rodel.peligro@asaphil.org>
2026-03-23 15:17:47 +0800

    yeahh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+   base: process.env.NODE_ENV === "production"
+    ? "/front_end_APECC_SYSTEM/"
+    : "/",
   server: {
     port: 5175, // Fixed port for consistent Google OAuth configuration
     strictPort: false, // Allow fallback if port is already in use
