99243c2633d8976805ef2148e7e201a3a18c5c19
IT-8437\RODEL <rodel.peligro@asaphil.org>
2026-04-22 16:30:38 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const basename = import.meta.env.PROD ? "/front_end_APECC_SYSTEM" : "/";
+const basename = import.meta.env.PROD ? "/front_end_APECC_SYSTEM/" : "/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
