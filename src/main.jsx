86fa9c4a32f5a3b9026a3b4b05ccf826f6a9939e
IT-8437\RODEL <rodel.peligro@asaphil.org>
2026-04-22 16:24:56 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -8,8 +8,8 @@ const basename = import.meta.env.PROD ? "/front_end_APECC_SYSTEM" : "/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename={basename}>
+    <BrowserRouter basename={basename}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
